d79cfd240aaa7c085dfd49124d90e099f80099e6
Alexandru Ionut Balan <alexandru.ionut.balan@proton.me>
2023-04-28 11:05:40 +0300

    Add jws header structure and default values

---
+++ jws/header.go
@@ -0,0 +1,25 @@
+package jws
+
+type JwsHeader struct {
+	B64  bool          `json:"b64"`
+	S256 string        `json:"x5t#S256"`
+	Crit []string      `json:"crit"`
+	SigT string        `json:"sigT"`
+	SigD SignedHeaders `json:"sigD"`
+	Alg  string        `json:"alg"`
+}
+
+type SignedHeaders struct {
+	Pars []string `json:"pars"`
+}
+
+func DefaultJwsHeader() *JwsHeader {
+	return &JwsHeader{
+		B64:  false,
+		Crit: []string{"sigT", "sigD", "b64"},
+		Alg:  "RS256",
+		SigD: SignedHeaders{
+			Pars: []string{"(request-target)", "digest"},
+		},
+	}
+}
